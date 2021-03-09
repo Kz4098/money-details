b2ea8488e8cae7f9cecf560f962e16d5993db4c2
EC2 Default User <ec2-user@ip-172-31-38-62.us-east-2.compute.internal>
2021-03-09 11:09:02 +0000

    fix footer and add viewport setting

--- resources/views/layouts/layouts.blade.php
+++ resources/views/layouts/layouts.blade.php
@@ -2,6 +2,7 @@
     <head>
         <title>@yield('title')</title>
         <meta name="csfr-token" content="{{ csfr-token() }}">
+           <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     </head>
     <body>
